d91503a5f3dc8d0d2e0413ccb4cdcab8a73b2320
rafaelwitter <rafawitt@hotmail.com>
2021-06-16 10:01:30 -0300

    Adicionado comentarios a users.service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,25 +1,32 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import User from './user.entity';
-// import { User, UserDocument, UserSchema } from './schemas/user.schema';
 import { CreateUserDto } from './dto/create-user.dto';
+
+// IMPORT PARA USER.ENTITY
+import User from './user.entity';
+
+// IMPORT PARA USER WITH SCHEMA
+// import { User, UserDocument, UserSchema } from './schemas/user.schema';
 // import { InjectModel } from '@nestjs/mongoose';
 // import { Model } from 'mongoose';
  
 @Injectable()
 export class UsersService {
+ // USER.ENTITY
   constructor(
     @InjectRepository(User)
     private usersRepository: Repository<User>
   ) {}
 
+ // USER && USER.SCHEMA
   // constructor(
   //   @InjectModel(User.name)
   //   private usersRepository: Model<User>
   // ) {}
  
- 
+
+ // ESTA COMENTADO PQ DA CONFLITO ENTRE ENTITY E SCHEMA
   // async getById(id: number) {
   //   const user = await this.usersRepository.findOne({ id });
   //   if (user) {
